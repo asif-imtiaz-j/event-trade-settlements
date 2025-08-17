7cfe384e3cca23d6f35deef5b2eabfe3539cd75a
asif-imtiaz-j <asifimtiaz@usf.edu>
2025-08-17 14:49:46 -0400

    feat(settlements): add Spring Boot entrypoint

---
+++ settlements-service/src/main/java/io/github/asifimtiazj/settlements/SettlementsServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.asifimtiazj.settlements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementsServiceApplication.class, args);
+    }
+}

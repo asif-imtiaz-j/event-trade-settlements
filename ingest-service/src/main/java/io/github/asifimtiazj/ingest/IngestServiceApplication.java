a7b42f50bc9fecb92f5cd802b126bacb7d25b001
asif-imtiaz-j <asifimtiaz@usf.edu>
2025-08-17 14:35:18 -0400

    feat(ingest): add Spring Boot entrypoint

---
+++ ingest-service/src/main/java/io/github/asifimtiazj/ingest/IngestServiceApplication.java
@@ -0,0 +1,11 @@
+﻿package io.github.asifimtiazj.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IngestServiceApplication.class, args);
+    }
+}
